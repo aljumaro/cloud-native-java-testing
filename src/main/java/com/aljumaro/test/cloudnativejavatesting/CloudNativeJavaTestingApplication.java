584d7dbc9c558beba3b54978f3aa0b53755d11b4
Juanma <aljumaro@gmail.com>
2017-10-12 00:41:49 +0200

    Initial commit

---
+++ src/main/java/com/aljumaro/test/cloudnativejavatesting/CloudNativeJavaTestingApplication.java
@@ -0,0 +1,12 @@
+package com.aljumaro.test.cloudnativejavatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeJavaTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeJavaTestingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aljumaro/test/cloudnativejavatesting/CloudNativeJavaTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aljumaro.test.cloudnativejavatesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudNativeJavaTestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
